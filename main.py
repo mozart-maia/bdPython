31ac6dd3ffff3e5c009e3b3c7404581c4dece4c3
Mozart Maia <mozartmaia89@gmail.com>
2024-10-22 19:00:00 -0300

    fix: minor changes

--- main.py
+++ main.py
@@ -1,51 +1,52 @@
 import database as bd
 
+
 class Prompt():
     def __init__(self):
-        banco = bd.DataBase()
-        while True:      
-            banco.print()
+        database = bd.DataBase()
+        while True:
+            database.print()
             command = input("> ")
             print(command)
-            commands =  command.split()
+            commands = command.split()
             if commands[0] == "exit":
-                    break
+                break
             if commands[0] == "print":
-                    banco.print_trans()
+                database.print_trans()
             if commands[0] == "set":
                 try:
-                    if banco.isTransactionOpen():
+                    if database.isTransactionOpen():
                         key = commands[1]
                         value = commands[2]
-                        banco.add_to_transaction(key, value)
+                        database.add_to_transaction(key, value)
                     else:
                         key = commands[1]
                         value = commands[2]
-                        banco.set(key, value)
+                        database.set(key, value)
                 except IndexError:
                     print("Usage: set <key> <value>")
-            if commands[0] ==  "get":
+            if commands[0] == "get":
                 try:
                     key = commands[1]
-                    result = banco.get(key)
+                    result = database.get(key)
                     print(result)
                 except IndexError:
-                    print("Usage: get <key> <value>")
+                    print("Usage: get <key>")
             if commands[0] == "begin":
-                banco.start_transaction()
+                database.start_transaction()
             if commands[0] == "rollback":
-                if banco.isTransactionOpen():
-                    banco.rollback()
+                if database.isTransactionOpen():
+                    database.rollback()
                 else:
                     print("No transaction opened yet")
             if commands[0] == "commit":
-                if banco.isTransactionOpen():   
-                    banco.commit()
+                if database.isTransactionOpen():
+                    database.commit()
                 else:
                     print("No transaction opened yet")
 
-            banco.save()
+            database.save()
 
-            
+
 if __name__ == "__main__":
     prompt = Prompt()
